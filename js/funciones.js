5c262d2907dbbecedb810aa1d0f0fcbd976b1169
Marcos Quevedo <quevedomarcos86@gmail.com>
2023-02-28 21:05:59 -0300

    Alojamiento del trabajo terminado

---
+++ js/funciones.js
@@ -0,0 +1,13 @@
+
+document.getElementById('dato').onclick = function () {
+    document.getElementById("nombre").innerHTML = "Peter Capusso, CEO, 522-chachacha";
+    
+    
+    }
+
+
+document.getElementById('datos').onclick = function () {
+document.getElementById("nombre").innerHTML = "Robertus Neutronius, Gerente, 252-521896";
+
+
+}
